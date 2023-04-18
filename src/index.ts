1c3c9282997a7fb811dfa0021f4bee2318bdabe5
whc <772778995@qq.com>
2023-04-18 08:46:31 +0800

    init

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+import windFlexPlugin from './tailwind/plugin/flex'
+import windLineClampPlugin from './tailwind/plugin/lineClamp'
+export { windFlexPlugin, windLineClampPlugin }
+
+import windPreset from './tailwind/preset'
+import windElementProPreset from './tailwind/preset/elementProPreset'
+export { windPreset, windElementProPreset }
